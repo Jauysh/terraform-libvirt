d447a05526fd748713b9623767f2f24e9c6fff6a
Jauysh <a.jauysh@gmail.com>
2025-03-17 10:03:29 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+# Use the existing default storage pool
+data "libvirt_pool" "default" {
+  name = "default"
+}
+
+# Create a volume from an existing QCOW2 image
+resource "libvirt_volume" "ubuntu-qcow2" {
+  name   = "ubuntu-22.04.qcow2"
+  pool   = data.libvirt_pool.default.name
+  source = "/var/lib/libvirt/images/ubuntu-22.04.qcow2"
+  format = "qcow2"
+}
+
+# Define the virtual machine
+resource "libvirt_domain" "ubuntu_vm" {
+  name   = "ubuntu-vm"
+  memory = 2048    # 2GB RAM
+  vcpu   = 2       # 2 vCPUs
+
+  disk {
+    volume_id = libvirt_volume.ubuntu-qcow2.id
+  }
+
+  network_interface {
+    network_name = "default"
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "vnc"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
